a9195c0f295e0b0f6222481343e6ccaa5ed42e98
JulietK <julietgisemba000@>
2020-10-11 06:51:14 +0300

    modifies files

--- app/src/main/java/com/moringaschool/linex_kenya/MainActivity.java
+++ app/src/main/java/com/moringaschool/linex_kenya/MainActivity.java
@@ -2,14 +2,28 @@ package com.moringaschool.linex_kenya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mGetStartedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGetStartedButton = (Button) findViewById(R.id.getStartedButton);
+        mGetStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LinexActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
